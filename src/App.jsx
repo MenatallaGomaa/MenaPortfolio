f55d5f90831018ef0761e8a0cdd7a243b1c9386f
Menatalla Gomaa <minafathi98@gmail.com>
2024-02-01 14:33:19 +0100

    uploading imgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-function (){
-  return ("Hello World!") 
-  
+import styles from "./App.module.css";
+
+function App() {
+  return <div className={styles.App}>Hello World!</div>;
 }
 
 export default App;
