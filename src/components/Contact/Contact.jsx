d890a9cf6bbb83b69dfc97792cc1791d67592bbb
Menatalla Gomaa <minafathi98@gmail.com>
2024-04-03 13:57:36 +0200

    Added clickable icons with corresponding URLs for email, LinkedIn, and GitHub.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,26 +12,25 @@ export const Contact = () => {
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/Email.png")} alt="Email icon" />
-          <a href="mailto:minafathi98@gmail.com">minafathi98@gmail.com</a>
+          <a href="mailto:minafathi98@gmail.com">
+            <img src={getImageUrl("contact/Email.png")} alt="Email icon" />
+          </a>
+          <a href="mailto:minafathi98@gmail.com"></a>
         </li>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/LinkedIn.png")} alt="LinkedIn icon" />
-          <a
-            href="https://www.linkedin.com/in/menatalla-gomaa-0703mg/
-"
-          >
-            https://www.linkedin.com/in/menatalla-gomaa-0703mg/
+          <a href="https://www.linkedin.com/in/menatalla-gomaa-0703mg/">
+            <img
+              src={getImageUrl("contact/LinkedIn.png")}
+              alt="LinkedIn icon"
+            />
           </a>
+          <a href="https://www.linkedin.com/in/menatalla-gomaa-0703mg/"></a>
         </li>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/Github.png")} alt="Github icon" />
-          <a
-            href="https://github.com/MenatallaGomaa
-"
-          >
-            https://github.com/MenatallaGomaa
+          <a href="https://github.com/MenatallaGomaa">
+            <img src={getImageUrl("contact/Github.png")} alt="Github icon" />
           </a>
+          <a href="https://github.com/MenatallaGomaa"></a>
         </li>
       </ul>
     </footer>
